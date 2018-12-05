0a37adc0cb10ce22fc4aee9f55882fd7626a34ea
Drew Miley <drew.miley@dwp.gsi.gov.uk>
2018-12-05 13:24:32 +0000

    Forms track data object

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ var stream = fs.createReadStream("./library.xml");
 let noSongs = 0
 
 parser.on("track", function(track) {
-  noSongs++;
-    console.log("track:", track);
+    // noSongs++;
+    const track1 = {
+        album: track['Album'],
+        artist: track['Artist'],
+        length: track['Total Time'],
+        name: track['Name'],
+        runningOrder: {
+            ...(track['Disc Count'] > 1 && { disc: track['Disc Number'] }),
+            number: track['Track Number']
+        }
+    }
+    if (track1.runningOrder.disc) {
+        noSongs++;
+        console.log("track name:", track1);
+    }
     console.log(noSongs);
 });
 

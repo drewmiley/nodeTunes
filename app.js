98dc349017aa120c8092ee6aa54f2c01e2c7564f
Drew Miley <drew.miley@dwp.gsi.gov.uk>
2018-12-05 11:57:43 +0000

    Implemented itunes-data

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('RUNNING');
+
+var fs = require("fs");
+var itunes = require("itunes-data");
+var parser = itunes.parser();
+var stream = fs.createReadStream("./library.xml");
+
+parser.on("track", function(track) {
+    console.log("track:", track);
+});
+
+parser.on("album", function(album) {
+    console.log("album:", album);
+});
+
+stream.pipe(parser);

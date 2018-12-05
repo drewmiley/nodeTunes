29b9216fb1f2bb67d246c5a7081b0bb1635471a0
Drew Miley <drew.miley@dwp.gsi.gov.uk>
2018-12-05 12:05:42 +0000

    Prints out number of songs- only tacks in xml

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ var itunes = require("itunes-data");
 var parser = itunes.parser();
 var stream = fs.createReadStream("./library.xml");
 
+let noSongs = 0
+
 parser.on("track", function(track) {
+  noSongs++;
     console.log("track:", track);
+    console.log(noSongs);
 });
-
-// parser.on("album", function(album) {
-//     console.log("album:", album);
-// });
-
-// parser.on("artist", function(artist) {
-//   console.log("artist:", artist);
-// });
 
 stream.pipe(parser);

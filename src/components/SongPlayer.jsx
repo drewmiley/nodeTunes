e40d858502a9735b6fa2e7e1cb5ecbb6be39d132
Drew Miley <drewmiley92@gmail.com>
2018-12-15 23:56:01 +0000

    Loads song player on initialisation

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -3,18 +3,22 @@ import React, { Component } from 'react';
 import { Howl, Howler } from 'howler';
 
 export default class SongPlayer extends Component {
-    playSong(song) {
-        const sound = new Howl({
-          src: [song.location]
+    constructor(props) {
+        super(props);
+        const song = new Howl({
+          src: [props.song.location]
         });
+        this.state = { song };
+    }
 
-        sound.play();
+    playSong() {
+        this.state.song.play();
     }
 
     render() {
         return (
             <div>
-                <button onClick={() => this.playSong(this.props.song)}>Play</button>
+                <button onClick={this.playSong.bind(this)}>Play</button>
             </div>
         );
     }

47de810402061b81225df353538de8f9d397f6f8
Drew Miley <drewmiley92@gmail.com>
2018-12-16 00:25:06 +0000

    Adds fast forward and rewind

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -25,12 +25,26 @@ export default class SongPlayer extends Component {
         this.state.song.stop(this.state.id);
     }
 
+    rewindSong() {
+        const seconds = this.state.song.seek();
+        this.state.song.seek(seconds - this.state.rewind, this.state.id);
+    }
+
+    fastforwardSong() {
+        const seconds = this.state.song.seek();
+        this.state.song.seek(seconds + this.state.fastforward, this.state.id);
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.playSong.bind(this)}>Play</button>
                 <button onClick={this.pauseSong.bind(this)}>Pause</button>
                 <button onClick={this.stopSong.bind(this)}>Stop</button>
+                <input type='text' placeholder='Rewind Seconds' onChange={(e) => this.setState({ rewind: parseInt(e.target.value) })}/>
+                <button disabled={!this.state.rewind} onClick={this.rewindSong.bind(this)}>{"<<"}</button>
+                <input type='text' placeholder='FastForward Seconds' onChange={(e) => this.setState({ fastforward: parseInt(e.target.value) })}/>
+                <button disabled={!this.state.fastforward} onClick={this.fastforwardSong.bind(this)}>{">>"}</button>
             </div>
         );
     }

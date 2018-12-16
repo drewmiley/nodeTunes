9dcc075dcea3eec3ac9938d28522633f994f3d71
Drew Miley <drewmiley92@gmail.com>
2018-12-16 00:06:27 +0000

    Implements pause/stop

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -12,13 +12,24 @@ export default class SongPlayer extends Component {
     }
 
     playSong() {
-        this.state.song.play();
+        const id = this.state.song.play(this.state.id);
+        this.setState({ id });
+    }
+
+    pauseSong() {
+        this.state.song.pause(this.state.id);
+    }
+
+    stopSong() {
+        this.state.song.stop(this.state.id);
     }
 
     render() {
         return (
             <div>
                 <button onClick={this.playSong.bind(this)}>Play</button>
+                <button onClick={this.pauseSong.bind(this)}>Pause</button>
+                <button onClick={this.stopSong.bind(this)}>Stop</button>
             </div>
         );
     }

405ca359add9c366906a4cb5b5b388675fcad7cc
Drew Miley <drewmiley92@gmail.com>
2018-12-20 23:59:37 +0000

    Cleans up actions

--- src/ducks/actions.js
+++ src/ducks/actions.js
@@ -1,46 +1,33 @@
-export const mapDispatchToProps = dispatch => {
-    return {
-        fetchData: url => dispatch(itemsFetchData(url)),
-        setSongPlayingId: songPlayingId => dispatch(setSongPlayingId(songPlayingId))
-    };
+export const mapDispatchToProps = dispatch => ({
+    fetchData: url => dispatch(itemsFetchData(url)),
+    setSongPlayingId: songPlayingId => dispatch(setSongPlayingId(songPlayingId))
+});
+
+const itemsFetchData = params => dispatch => {
+    let url = `http://localhost:8000/api/songs/title/${ params.title }?sortBy=${ params.sortBy }`;
+    if (params.artist) {
+        url += `&artist=${ params.artist }`;
+    }
+    if (params.album) {
+        url += `&album=${ params.album }`;
+    }
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+
+            return response;
+        })
+        .then(response => response.json())
+        .then(response => response.results)
+        .then(items => dispatch(itemsFetchDataSuccess(items)));
 };
 
-function itemsFetchData(params) {
-    return dispatch => {
-        let url = `http://localhost:8000/api/songs/title/${ params.title }?sortBy=${ params.sortBy }`;
-        if (params.artist) {
-            url += `&artist=${ params.artist }`;
-        }
-        if (params.album) {
-            url += `&album=${ params.album }`;
-        }
+const itemsFetchDataSuccess = items => ({
+    type: 'ITEMS_FETCH_DATA_SUCCESS',
+    items
+});
 
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-
-                return response;
-            })
-            .then(response => response.json())
-            .then(response => response.results)
-            .then(items => dispatch(itemsFetchDataSuccess(items)));
-    };
-}
-
-function itemsFetchDataSuccess(items) {
-    return {
-        type: 'ITEMS_FETCH_DATA_SUCCESS',
-        items
-    };
-}
-
-function setSongPlayingId(songPlayingId) {
-    return dispatch => {
-        dispatch({
-            type: 'SET_SONG_PLAYING_ID',
-            songPlayingId
-        })
-    };
-}
+const setSongPlayingId = songPlayingId => dispatch => dispatch({ type: 'SET_SONG_PLAYING_ID', songPlayingId });

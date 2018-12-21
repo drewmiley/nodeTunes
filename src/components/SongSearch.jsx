5336cd8bd73fa6e7d0e5c331b1305df49eeddc9d
Drew Miley <drew.miley@dwp.gsi.gov.uk>
2018-12-21 11:37:43 +0000

    Cleans up SongSearch

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -1,48 +1,25 @@
 import React, { useState } from 'react';
 
-const useComponentState = props => {
-  const [artist, setArtist] = useState(null);
-  const [album, setAlbum] = useState(null);
-  const [sortBy, setSortBy] = useState('title');
-  const [title, setTitle] = useState(null);
-
-  const fetchData = () => {
-    props.fetchData({
-      artist,
-      album,
-      sortBy,
-      title
-    })
-  }
-
-  return {
-    artist,
-    setArtist,
-    album,
-    setAlbum,
-    sortBy,
-    setSortBy,
-    title,
-    setTitle,
-    fetchData
-  }
-}
-
 const SongSearch = props => {
-    const state = useComponentState(props);
+    const [artist, setArtist] = useState(null);
+    const [album, setAlbum] = useState(null);
+    const [sortBy, setSortBy] = useState('title');
+    const [title, setTitle] = useState(null);
+
+    const fetchData = () => props.fetchData({ artist, album, sortBy, title });
 
     return (
         <div>
-            <input type='text' placeholder='Artist' onChange={(e) => state.setArtist(e.target.value)}/>
-            <input type='text' placeholder='Album' onChange={(e) => state.setAlbum(e.target.value)}/>
-            <input type='text' placeholder='Title' onChange={(e) => state.setTitle(e.target.value)}/>
-            <select defaultValue={state.sortBy} onChange={(e) => state.setSortBy(e.target.value)}>
+            <input type='text' placeholder='Artist' onChange={(e) => setArtist(e.target.value)}/>
+            <input type='text' placeholder='Album' onChange={(e) => setAlbum(e.target.value)}/>
+            <input type='text' placeholder='Title' onChange={(e) => setTitle(e.target.value)}/>
+            <select defaultValue={sortBy} onChange={(e) => setSortBy(e.target.value)}>
                 <option value="album">Album</option>
                 <option value="artist">Artist</option>
                 <option value="length">Length</option>
                 <option value="title">Title</option>
             </select>
-            <button onClick={state.fetchData}>Fetch Songs</button>
+            <button onClick={fetchData}>Fetch Songs</button>
         </div>
     );
 }

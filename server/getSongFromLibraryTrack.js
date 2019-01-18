a3dd279afdb101dd93bbc7922b4406ee6a6e5ed8
Drew Miley <drew.miley@opencastsoftware.com>
2019-01-18 15:08:29 +0000

    Add song converter

--- server/getSongFromLibraryTrack.js
+++ server/getSongFromLibraryTrack.js
@@ -1,4 +1,14 @@
-module.exports = libraryTrack => {
-    console.log(libraryTrack);
-    return libraryTrack;
+module.exports = track => {
+    const song = {
+        album: track['Album'],
+        artist: track['Artist'],
+        length: track['Total Time'],
+        location: track['Location'].replace('file:///Users/drewmiley/Music/iTunes', 'http://localhost:3000'),
+        runningOrder: {
+            ...(track['Disc Count'] > 1 && { disc: track['Disc Number'] }),
+            number: track['Track Number']
+        },
+        title: track['Name']
+    }
+    return song;
 }

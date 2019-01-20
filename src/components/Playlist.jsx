6621dca083d0bec4c6e8160d74e8572a4f8174a9
Drew Miley <drewmiley92@gmail.com>
2019-01-20 14:14:33 +0000

    WIP on drag and drop

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -7,60 +7,52 @@ import SongInfo from './SongInfo';
 // const PlaylistSong = SortableElement(props => {
 //     console.log(props);
 //     return (
-//         <div key={`playlistSong-${ props.i }`} className={`songContainer ${ props.i === props.playlistSongPlayingIndex ? 'playlistSongPlaying' : '' }`}>
+//         <div className={`songContainer ${ props.i === props.playlistSongPlayingIndex ? 'playlistSongPlaying' : '' }`}>
 //             <SongInfo song={props.song} />
 //             <button onClick={() => props.removeSongFromPlaylist(props.i)}>Remove Song From Playlist</button>
 //         </div>
 //     )
 // });
-//
-// const SortablePlaylist = SortableContainer(props => {
-//     console.log(props);
-//     return (
-//         <>
-//             {props.songs.map((song, i) => (
-//                 <PlaylistSong
-//                     song={song}
-//                     index={i}
-//                     i={i}
-//                     playlistSongPlayingIndex={props.playlistSongPlayingIndex}
-//                     setPlaylistSongPlayingIndex={props.setPlaylistSongPlayingIndex}
-//                     removeSongFromPlaylist={props.removeSongFromPlaylist}
-//                 />
-//             ))}
-//         </>
-//     )
-// });
-//
-// class PlaylistSorter extends Component {
-//     onSortEnd() {
-//         console.log('hello');
-//     }
-//
-//     render() {
-//         return <SortablePlaylist {...this.props} onSortEnd={this.onSortEnd} />
-//     }
-// }
-//
-// class Playlist extends Component {
-//     render() {
-//     return (
-//         <>
-//             <h1>Playlist</h1>
-//             <PlaylistPlayer
-//                 songs={this.props.songs}
-//                 songPlayingId={this.props.songPlayingId}
-//                 setSongPlayingId={this.props.setSongPlayingId}
-//                 playlistSongPlayingIndex={this.props.playlistSongPlayingIndex}
-//                 setPlaylistSongPlayingIndex={this.props.setPlaylistSongPlayingIndex}
-//             />
-//         <PlaylistSorter {...this.props} />
-//         </>
-//     );
-//     }
-// }
-//
+
 const SortableItem = SortableElement(({value}) => <li>{value}</li>);
+
+const SortablePlaylist = SortableContainer(({items}) => {
+    console.log(items)
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={index} />
+      ))}
+    </ul>
+  );
+});
+
+// const SortablePlaylist = SortableContainer(({items}) => {
+//     console.log(items);
+//     return (<div></div>)
+//     return (
+//         <div>
+//             {items.map((song, i) => (
+//                 <SortableItem key={`item-${i}`} index={i} value={song} />
+//             ))}
+//         <div/>
+//     )
+//     // return (
+//     //     <>
+//     //         {props.songs.map((song, i) => (
+//     //             <PlaylistSong
+//     //                 key={i}
+//     //                 song={song}
+//     //                 index={i}
+//     //                 i={i}
+//     //                 playlistSongPlayingIndex={props.playlistSongPlayingIndex}
+//     //                 setPlaylistSongPlayingIndex={props.setPlaylistSongPlayingIndex}
+//     //                 removeSongFromPlaylist={props.removeSongFromPlaylist}
+//     //             />
+//     //         ))}
+//     //     </>
+//     // )
+// });
 
 const SortableList = SortableContainer(({items}) => {
   return (
@@ -73,19 +65,36 @@ const SortableList = SortableContainer(({items}) => {
 });
 
 class SortableComponent extends Component {
-  state = {
-    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
-  };
-  onSortEnd = ({oldIndex, newIndex}) => {
-    this.setState(({items}) => ({
-      items: arrayMove(items, oldIndex, newIndex),
-    }));
-  };
-  render() {
-    return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
-  }
+    state = {
+        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    };
+
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState(({items}) => ({
+            items: arrayMove(items, oldIndex, newIndex),
+        }));
+    };
+
+    onSortEnd1 = () => {
+        console.log('hello');
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Playlist</h1>
+                <PlaylistPlayer
+                    songs={this.props.songs}
+                    songPlayingId={this.props.songPlayingId}
+                    setSongPlayingId={this.props.setSongPlayingId}
+                    playlistSongPlayingIndex={this.props.playlistSongPlayingIndex}
+                    setPlaylistSongPlayingIndex={this.props.setPlaylistSongPlayingIndex}
+                />
+            <SortablePlaylist items={this.props.songs} onSortEnd={this.onSortEnd1} />
+            <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
+            </>
+        );
+    }
 }
-//
-// render(<SortableComponent />, document.getElementById('root'));
 
 export default SortableComponent;

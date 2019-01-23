3d9ec217f3e0bf9a618b6d18ac8f968faec7e4b0
Drew Miley <drew.miley@opencastsoftware.com>
2019-01-23 14:24:50 +0000

    Sets env in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,38 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  target: "web",
-  mode: "development",
-  devtool: "source-map",
-  entry: "./index.jsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "app.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/env", "@babel/preset-react"]
+module.exports = env => {
+  return {
+    target: "web",
+    mode: "development",
+    devtool: "source-map",
+    entry: "./index.jsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "app.js",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/env", "@babel/preset-react"]
+            }
           }
         }
-      }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.API_URL': JSON.stringify(env.API_URL || 'http://localhost:8000')
+      }),
+      new CopyWebpackPlugin(["index.html"])
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.API_URL': JSON.stringify(process.env.API_URL || 'http://localhost:8000')
-    }),
-    new CopyWebpackPlugin(["index.html"])
-  ]
+  }
 };

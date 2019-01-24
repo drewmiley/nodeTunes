9ab491bd536d28fb6955ac2e058e19e6d789fcf0
Drew Miley <drewmiley92@gmail.com>
2019-01-24 00:40:27 +0000

    Adds loading screen

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -7,16 +7,26 @@ import SongSearch from './components/SongSearch';
 import { mapDispatchToProps } from './ducks/actions';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
+    }
+
     componentDidMount() {
         if (window.location.search.substring(1)) {
             console.log(`loading from ${ window.location.search.substring(1) }`);
             fetch(`${ process.env.API_URL }/api/loadNewLibrary?url=${ window.location.search.substring(1) }`)
                 .then(res => res.json())
-                .then(console.log);
+                .then(console.log)
+                .then(() => this.setState({ loading: false }));
         }
     }
 
     render() {
+        if (this.state.loading) {
+            return <h1>Loading from { window.location.search.substring(1) }</h1>
+        }
+
         return <>
             <div className='half-width'>
                 <SongSearch

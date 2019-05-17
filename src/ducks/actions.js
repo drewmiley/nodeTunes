a550dbfd4717a655412809935a84015b63d2b3a4
Drew Miley <drew.miley@opencastsoftware.com>
2019-05-17 13:21:51 +0100

    Some clean up of fetch actions

--- src/ducks/actions.js
+++ src/ducks/actions.js
@@ -1,69 +1,42 @@
 import * as actiontypes from './actiontypes';
 
-const songsFetchData = params => dispatch => {
+const fetchSongs = params => dispatch => {
+    const songsFetchDataSuccess = songs => ({
+        type: actiontypes.SONGS_FETCH_DATA_SUCCESS,
+        songs
+    });
+
     let url = `${ process.env.API_URL }/api/songs/title/${ params.title }?sortBy=${ params.sortBy }`;
-    if (params.artist) {
-        url += `&artist=${ params.artist }`;
-    }
-    if (params.album) {
-        url += `&album=${ params.album }`;
-    }
+    if (params.artist) { url += `&artist=${ params.artist }`; };
+    if (params.album) { url += `&album=${ params.album }`; };
 
     fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-
-            return response;
-        })
         .then(response => response.json())
         .then(response => response.results)
         .then(songs => dispatch(songsFetchDataSuccess(songs)));
 };
 
-const songsFetchDataSuccess = songs => ({
-    type: actiontypes.SONGS_FETCH_DATA_SUCCESS,
-    songs
-});
-
 const fetchArtists = () => dispatch => {
+    const artistsFetchDataSuccess = artists => ({
+        type: actiontypes.ARTISTS_FETCH_DATA_SUCCESS,
+        artists
+    });
     fetch(`${ process.env.API_URL }/api/artists`)
-        .then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-
-            return response;
-        })
         .then(response => response.json())
-        .then(response => response.results)
-        .then(artists => dispatch(artistsFetchDataSuccess(artists.map(artist => artist.name))));
+        .then(response => response.results.map(artist => artist.name))
+        .then(artists => dispatch(artistsFetchDataSuccess(artists)));
 };
-
-const artistsFetchDataSuccess = artists => ({
-    type: actiontypes.ARTISTS_FETCH_DATA_SUCCESS,
-    artists
-});
 
 const fetchAlbums = () => dispatch => {
+    const albumsFetchDataSuccess = albums => ({
+        type: actiontypes.ALBUMS_FETCH_DATA_SUCCESS,
+        albums
+    });
     fetch(`${ process.env.API_URL }/api/albums`)
-        .then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-
-            return response;
-        })
         .then(response => response.json())
-        .then(response => response.results)
-        .then(albums => dispatch(albumsFetchDataSuccess(albums.map(album => album.title))));
+        .then(response => response.results.map(album => album.title))
+        .then(albums => dispatch(albumsFetchDataSuccess(albums)));
 };
-
-const albumsFetchDataSuccess = albums => ({
-    type: actiontypes.ALBUMS_FETCH_DATA_SUCCESS,
-    albums
-});
 
 const setSongPlayingId = songPlayingId => dispatch => dispatch({ type: actiontypes.SET_SONG_PLAYING_ID, songPlayingId });
 
@@ -75,7 +48,7 @@ const setPlaylistSongPlayingIndex = index => dispatch => dispatch({type: actiont
 const setPlaylist = songs => dispatch => dispatch({type: actiontypes.SET_PLAYLIST, songs});
 
 export const mapDispatchToProps = dispatch => ({
-    fetchData: params => dispatch(songsFetchData(params)),
+    fetchData: params => dispatch(fetchSongs(params)),
     setSongPlayingId: songPlayingId => dispatch(setSongPlayingId(songPlayingId)),
     addSongToPlaylist: song => dispatch(addSongToPlaylist(song)),
     removeSongFromPlaylist: index => dispatch(removeSongFromPlaylist(index)),

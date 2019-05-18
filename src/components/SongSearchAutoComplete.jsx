5701643c64b61fdaf99da907c6df8f9e5c75f123
Drew Miley <drewmiley92@gmail.com>
2019-05-18 13:48:07 +0100

    Styles on autocomplete

--- src/components/SongSearchAutoComplete.jsx
+++ src/components/SongSearchAutoComplete.jsx
@@ -3,17 +3,19 @@ import AutoComplete from 'react-autocomplete';
 
 const SongSearchAutoComplete = props => {
     return (
-        <AutoComplete
-            getItemValue={item => item}
-            items={props.items}
-            renderItem={(item, isHighlighted) =><div style={{ fontSize: '30px', background: isHighlighted ? 'lightgray' : 'white' }} key={item}>{item}</div>}
-            shouldItemRender={(item, value) => item && item.toUpperCase().includes(value.toUpperCase())}
-            value={props.value}
-            inputProps={{ placeholder: props.placeholder }}
-            menuStyle={{ width: '45%' }}
-            onChange={e => props.setValue(e.target.value)}
-            onSelect={val => props.setValue(val)}
-        />
+        <div style={{ display: 'list-item' }}>
+            <AutoComplete
+                getItemValue={item => item}
+                items={props.items}
+                renderItem={(item, isHighlighted) =><div style={{ fontSize: '30px', background: isHighlighted ? 'lightgray' : 'white' }} key={item}>{item}</div>}
+                shouldItemRender={(item, value) => item && item.toUpperCase().includes(value.toUpperCase())}
+                value={props.value}
+                inputProps={{ placeholder: props.placeholder }}
+                menuStyle={{ width: '45%' }}
+                onChange={e => props.setValue(e.target.value)}
+                onSelect={val => props.setValue(val)}
+            />
+        </div>
     )
 }
 

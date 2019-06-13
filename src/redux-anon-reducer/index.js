85aa7d6d06340c4bfa2ae6838221c999ee400a07
Drew Miley <drew.miley@opencastsoftware.com>
2019-06-13 21:25:01 +0100

    Playlist song playing index works

--- src/redux-anon-reducer/index.js
+++ src/redux-anon-reducer/index.js
@@ -2,5 +2,5 @@ export default (actionMap, initialReturn = null, defaultReturn = undefined) =>
     (state, action) =>
         (actionMap[action.type] || ( d => () => (defaultReturn === undefined ? d : defaultReturn)))
         //             (state === Object(state) ? Object.assign({}, state) : state)
-            (state || initialReturn)
+            (state !== undefined ? state : initialReturn)
                 (action.payload);

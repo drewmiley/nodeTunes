705bcd14961390b025d79d6641f1ac82d120bdac
Drew Miley <drewmiley92@gmail.com>
2019-08-12 20:32:17 +0100

    Hookifies AppContainer

--- src/AppContainer.jsx
+++ src/AppContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import Playlist from './components/Playlist';
@@ -6,62 +6,59 @@ import SongList from './components/SongList';
 import SongSearch from './components/SongSearch';
 import { mapDispatchToProps } from './ducks/actions';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { loading: true };
-    }
+const App = props => {
+    const [loading, setLoading] = useState(true);
 
-    componentDidMount() {
-        let url = `${ process.env.API_URL }/api/`;
-        if (window.location.search.substring(1)) {
-            console.log(`loading from ${ window.location.search.substring(1) }`);
-            url += `loadNewLibrary?url=${ window.location.search.substring(1) }`;
-        } else {
-            console.log('clearing library');
-            url += 'clearLibrary';
+    useEffect(() => {
+        if (loading) {
+            let url = `${ process.env.API_URL }/api/`;
+            if (window.location.search.substring(1)) {
+                console.log(`loading from ${ window.location.search.substring(1) }`);
+                url += `loadNewLibrary?url=${ window.location.search.substring(1) }`;
+            } else {
+                console.log('clearing library');
+                url += 'clearLibrary';
+            }
+            fetch(url)
+                .then(res => res.json())
+                .then(console.log)
+                .then(props.fetchArtists)
+                .then(props.fetchAlbums)
+                .then(() => setLoading(false));
         }
-        fetch(url)
-            .then(res => res.json())
-            .then(console.log)
-            .then(this.props.fetchArtists)
-            .then(this.props.fetchAlbums)
-            .then(() => this.setState({ loading: false }));
+    });
+
+    if (loading) {
+        return <h1>Loading from { window.location.search.substring(1) }</h1>
     }
 
-    render() {
-        if (this.state.loading) {
-            return <h1>Loading from { window.location.search.substring(1) }</h1>
-        }
-
-        return <>
-            <div className='half-width'>
-                <SongSearch
-                    artists={this.props.artists}
-                    albums={this.props.albums}
-                    fetchData={this.props.fetchData}
-                />
-                <SongList
-                    songs={this.props.songs}
-                    playlistSongs={this.props.playlistSongs}
-                    songPlayingId={this.props.songPlayingId}
-                    setSongPlayingId={this.props.setSongPlayingId}
-                    addSongToPlaylist={this.props.addSongToPlaylist}
-                />
-            </div>
-            <div className={`half-width ${ this.props.playlistSongs.length === 0 ? 'emptyPlaylist' : 'playlist' }`}>
-                <Playlist
-                    songs={this.props.playlistSongs}
-                    songPlayingId={this.props.songPlayingId}
-                    setSongPlayingId={this.props.setSongPlayingId}
-                    removeSongFromPlaylist={this.props.removeSongFromPlaylist}
-                    setPlaylistSongPlayingIndex={this.props.setPlaylistSongPlayingIndex}
-                    playlistSongPlayingIndex={this.props.playlistSongPlayingIndex}
-                    setPlaylist={this.props.setPlaylist}
-                />
-            </div>
-        </>
-    }
-};
+    return <>
+        <div className='half-width'>
+            <SongSearch
+                artists={props.artists}
+                albums={props.albums}
+                fetchData={props.fetchData}
+            />
+            <SongList
+                songs={props.songs}
+                playlistSongs={props.playlistSongs}
+                songPlayingId={props.songPlayingId}
+                setSongPlayingId={props.setSongPlayingId}
+                addSongToPlaylist={props.addSongToPlaylist}
+            />
+        </div>
+        <div className={`half-width ${ props.playlistSongs.length === 0 ? 'emptyPlaylist' : 'playlist' }`}>
+            <Playlist
+                songs={props.playlistSongs}
+                songPlayingId={props.songPlayingId}
+                setSongPlayingId={props.setSongPlayingId}
+                removeSongFromPlaylist={props.removeSongFromPlaylist}
+                setPlaylistSongPlayingIndex={props.setPlaylistSongPlayingIndex}
+                playlistSongPlayingIndex={props.playlistSongPlayingIndex}
+                setPlaylist={props.setPlaylist}
+            />
+        </div>
+    </>
+}
 
 export default connect(state => state, mapDispatchToProps)(App);

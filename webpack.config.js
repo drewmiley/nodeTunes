3dbbbcfed9b72bc58a6ffd4db580642aed6e1c02
Drew Miley <drewmiley92@gmail.com>
2020-07-01 00:34:23 +0100

    Initial PWA work

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
@@ -33,7 +34,17 @@ module.exports = env => {
       new webpack.DefinePlugin({
         'process.env.API_URL': JSON.stringify(env.API_URL)
       }),
-      new CopyWebpackPlugin(["src/index.html"])
+      new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true
+      }),
+      new CopyWebpackPlugin([
+        { from: 'src/images', to: 'images' },
+        { from: 'src/manifest', to: 'manifest' },
+        { from: 'src/index.html', to: 'index.html' }
+      ])
     ]
   }
 };
